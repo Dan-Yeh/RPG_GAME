6b8b572b5591adad2b0b923e91480bc1c02a3087
ShengChiaYu <43724671+ShengChiaYu@users.noreply.github.com>
2021-02-14 16:43:40 +0100

    modify bot TEST

--- include/Bot.h
+++ include/Bot.h
@@ -9,15 +9,17 @@
 
 class Bot {
 public:
-    std::string behavior_f1 = "n Dan show fight rest fight rest fight fight 2 show fight save q";
 #ifdef TEST
 //For gprof to test perfomance
-    std::string behavior_f2 = "o Dan show fight rest fight rest fight show fight save q";
-    std::string behavior_m1 = "n Fan show fight rest fight rest fight fight 3 show fight save q";
-    std::string behavior_m2 = "o Fan show fight rest fight rest fight fight show fight save q";
-    std::string behavior_t1 = "n Ian show fight rest fight rest fight fight 1 show fight save q";
-    std::string behavior_t2 = "o Ian show fight rest fight rest fight fight show fight save q";
+    std::string behavior_f1 = "Dan show fight rest fight rest fight fight 2 show fight save q";
+    std::string behavior_f2 = "Dan show fight rest fight rest fight show fight save q";
+    std::string behavior_m1 = "Fan show fight rest fight rest fight fight 3 show fight save q";
+    std::string behavior_m2 = "Fan show fight rest fight rest fight fight show fight save q";
+    std::string behavior_t1 = "Ian show fight rest fight rest fight fight 1 show fight save q";
+    std::string behavior_t2 = "Ian show fight rest fight rest fight fight show fight save q";
     std::vector<std::string> behaviors { behavior_f1, behavior_f2, behavior_m1, behavior_m2, behavior_t1, behavior_t2 };
+#else
+    std::string behavior_f1 = "Dan show fight rest fight rest fight fight 2 show fight save q";
 #endif
 public:
     Bot();
